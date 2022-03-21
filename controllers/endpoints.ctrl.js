86de4dd187105498ac8322e0f3c55f816f4b1361
Danityec <Danityec@gmail.com>
2022-03-21 19:54:43 +0200

    update

--- controllers/endpoints.ctrl.js
+++ controllers/endpoints.ctrl.js
@@ -1,24 +1,66 @@
-let yaw = 0, throttle = 0, roll = 0, pitch = 0;
+// let yaw = 0, throttle = 0, roll = 0, pitch = 0;
+
+// exports.controller = {
+//     getRemote(req, res) {
+//         yaw = req.query.yaw ? req.query.yaw : yaw
+//         throttle = req.query.throttle ? req.query.throttle : throttle
+//         roll = req.query.roll ? req.query.roll : roll
+//         pitch = req.query.pitch ? req.query.pitch : pitch
+//         res.sendStatus(200);  
+        
+        
+//     },
+
+//     sendRemote(req, res) {
+//         res.set({
+//             "Content-Type": "application/json",
+//             "Access-Control-Allow-Origin": "*",
+//         });
+//         res.json({
+//             "yaw":yaw,
+//             "throttle":throttle,
+//             "roll":roll,
+//             "pitch":pitch
+//         })
+
+
+//     }
+// };
+
+
+const { response } = require('express');
+const axios = require('axios');
 
 exports.controller = {
     getRemote(req, res) {
-        yaw = req.query.yaw ? req.query.yaw : yaw
-        throttle = req.query.throttle ? req.query.throttle : throttle
-        roll = req.query.roll ? req.query.roll : roll
-        pitch = req.query.pitch ? req.query.pitch : pitch
-        res.sendStatus(200);   
+       
+        axios.get(`https://dronet-simulation.herokuapp.com/api/remote/get?yaw=${req.query.yaw}&throttle=${req.query.throttle}&roll=${req.query.roll}&pitch=${req.query.pitch}`)
+        .then(function (response) {
+            res.sendStatus(200); 
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
     },
 
     sendRemote(req, res) {
-        res.set({
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        });
-        res.json({
-            "yaw":yaw,
-            "throttle":throttle,
-            "roll":roll,
-            "pitch":pitch
+
+        axios.get(`https://dronet-simulation.herokuapp.com/api/remote/send`)
+        .then(function (response) {
+            let data = response
+            res.set({
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            });
+            res.json({
+                "yaw":data.yaw,
+                "throttle":data.throttle,
+                "roll":data.roll,
+                "pitch":data.pitch
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
         })
     }
 };
